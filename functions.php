811585be95410d6ed9ad0554165596d650e8041a
Manfred <marodok@gmail.com>
2018-08-03 14:48:23 -0600

    Revisión

--- functions.php
+++ functions.php
@@ -4,3 +4,11 @@ function padma_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+/*	Allow updates	*/
+if (is_admin()) {
+	if(class_exists('PadmaUpdater')){
+		$PadmaUpdater = new PadmaUpdater();
+		$PadmaUpdater->updater('padma-child',__DIR__,true);
+	}
+}
