ec113b8384bf487ce7a0500d564e642d12367895
Manfred <marodok@gmail.com>
2018-09-05 11:49:10 -0600

    Fix

--- functions.php
+++ functions.php
@@ -1,14 +1,7 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'padma_child_enqueue_styles' );
+
 function padma_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
-}
-
-/*	Allow updates	*/
-if (is_admin()) {
-	if(class_exists('PadmaUpdater')){
-		$PadmaUpdater = new PadmaUpdater();
-		$PadmaUpdater->updater('padma-child',__DIR__,true);
-	}
 }
